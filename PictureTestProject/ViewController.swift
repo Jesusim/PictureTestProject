a4e4ab8a66430c2d372468b01f68e377ef5b69d3
Dmitry <jesusimtoday@gmail.com>
2020-07-22 16:09:20 +0700

    Initial Commit

---
+++ PictureTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PictureTestProject
+//
+//  Created by Admin on 7/22/20.
+//  Copyright © 2020 BadJesus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
